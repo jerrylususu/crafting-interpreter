fb3cb834e0db774ad4fe15839ce455bb7ea7d1be
Neko Null <jerrylu.mail@qq.com>
2022-05-05 18:20:26 +0300

    9.2 if fix

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -198,7 +198,7 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         // affects side effect
         if(isTruthy(evaluate(stmt.condition))) {
             execute(stmt.thenBranch);
-        } else {
+        } else if (stmt.elseBranch != null) {
             execute(stmt.elseBranch);
         }
         return null;
@@ -209,6 +209,15 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         Object value = evaluate(stmt.expression);
         System.out.println(stringify(value));
         return null;
+    }
+
+    @Override
+    public Void visitReturnStmt(Stmt.Return stmt) {
+        // eval return value if present
+        Object value = null;
+        if (stmt.value != null) value = evaluate(stmt.value);
+
+        throw new Return(value);
     }
 
     @Override
