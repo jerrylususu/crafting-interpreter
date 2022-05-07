eb02013791bb5ea1616694aac31f185f0e48b26d
Neko Null <jerrylu.mail@qq.com>
2022-05-07 16:27:56 +0300

    12.3 Creating Instances

--- src/com/craftinginterpreters/lox/LoxClass.java
+++ src/com/craftinginterpreters/lox/LoxClass.java
@@ -6,7 +6,7 @@ import java.util.Map;
 /**
  * Runtime representation of a class
  */
-class LoxClass {
+class LoxClass implements LoxCallable{
     final String name;
 
     LoxClass(String name) {
@@ -16,5 +16,23 @@ class LoxClass {
     @Override
     public String toString() {
         return name;
+    }
+
+    /**
+     * When calling on class itself, creates a new instance of the class and returns it
+     * @param interpreter
+     * @param arguments
+     * @return
+     */
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        LoxInstance instance = new LoxInstance(this);
+        return instance;
+    }
+
+    @Override
+    public int arity() {
+        // no user-defined ctor for now
+        return 0;
     }
 }
