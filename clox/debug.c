7dba833cec8bc6d78feb6662c844616ed866f2c0
Neko Null <jerrylu.mail@qq.com>
2022-05-12 21:26:28 +0300

    14.4 Disassembling Chunks

---
+++ clox/debug.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#include "debug.h"
+
+void disassembleChunk(Chunk* chunk, const char* name) {
+    printf("== %s ==\n", name);
+
+    for (int offset = 0; offset < chunk->count;) {
+        // return the offset of next instruction
+        // instruction can have different sizes
+        offset = disassembleInstruction(chunk, offset);
+    }
+}
+
+// static: function is only visible to other functions in the same file (translation unit)
+static int simpleInstruction(const char* name, int offset) {
+    printf("%s\n", name);
+    return offset + 1;
+}
+
+int disassembleInstruction(Chunk* chunk, int offset) {
+    printf("%04d ", offset);
+
+    uint8_t instruction = chunk->code[offset];
+    switch (instruction) {
+        case OP_RETURN:
+            return simpleInstruction("OP_RETURN", offset);
+        default:
+            printf("Unknown opcode %d\n", instruction);
+            return offset + 1;
+    }
+}
