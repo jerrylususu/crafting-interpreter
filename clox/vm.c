098b77db85f35e515f0594bee9356910cfc1d09f
Neko Null <jerrylu.mail@qq.com>
2022-05-13 21:23:57 +0300

    15.1 An Instruction Execution Machine

---
+++ clox/vm.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#include "common.h"
+#include "debug.h"
+#include "vm.h"
+
+VM vm;
+
+void initVM() {
+
+}
+
+void freeVM() {
+
+}
+
+static InterpretResult run() {
+// make that scoping more explicit
+
+// read the byte and advance
+// note: ip advanced after reading and before executing
+#define READ_BYTE() (*vm.ip++)
+
+// read next byte and use it as index to look up the constant
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+
+    for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
+
+        uint8_t instruction;
+        // decoding / dispatching
+        switch (instruction = READ_BYTE()) {
+            case OP_CONSTANT: {
+                Value constant = READ_CONSTANT();
+                printValue(constant);
+                printf("\n");
+                break;
+            }
+            case OP_RETURN: {
+                return INTERPRET_OK;
+            }
+        }
+    }
+
+#undef READ_BYTE
+#undef READ_CONSTANT
+}
+
+InterpretResult interpret(Chunk* chunk) {
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
+}
