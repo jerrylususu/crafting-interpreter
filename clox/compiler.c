5a217a4eb8e51cee31c65e15d98240d2da5516b8
Neko Null <jerrylu.mail@qq.com>
2022-05-14 14:20:41 +0300

    16.2 A Token at a Time

--- clox/compiler.c
+++ clox/compiler.c
@@ -6,4 +6,21 @@
 
 void compile(const char* source) {
     initScanner(source);
+    int line = -1;
+    for (;;) {
+        Token token = scanToken();
+        if (token.line != line) {
+            printf("%4d ", token.line);
+            line = token.line;
+        } else {
+            printf("   | ");
+        }
+
+        // print the token
+        // %.*s allows the length (precision) to be dynamically controlled
+        // token is just offset into the source char array, no null terminator
+        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+        if (token.type == TOKEN_EOF) break;
+    }
 }
