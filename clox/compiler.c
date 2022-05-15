7e964d098f2f53d5ab144ee24c127a1423df2063
Neko Null <jerrylu.mail@qq.com>
2022-05-15 21:26:38 +0300

    17.2 Parsing Tokens

--- clox/compiler.c
+++ clox/compiler.c
@@ -1,27 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char* source) {
-    initScanner(source);
-    int line = -1;
-    for (;;) {
-        Token token = scanToken();
-        if (token.line != line) {
-            printf("%4d ", token.line);
-            line = token.line;
-        } else {
-            printf("   | ");
-        }
+typedef struct {
+    Token current;
+    Token previous;
+    bool hadError; // have compile time error
+    bool panicMode; // error will be suppressed if in panic mode, ends when the parser reach sync point
+} Parser;
 
-        // print the token
-        // %2d: token type index
-        // %.*s allows the length (precision) to be dynamically controlled
-        // token is just offset into the source char array, no null terminator
-        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+Parser parser;
 
-        if (token.type == TOKEN_EOF) break;
+static void errorAt(Token* token, const char* message) {
+    // if in panic mode, suppress error
+    if (parser.panicMode) return;
+    // get into panic mode when an error occurs
+    parser.panicMode = true;
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TOKEN_EOF) {
+        fprintf(stderr, " at end");
+    } else if (token->type == TOKEN_ERROR) {
+        // Nothing.
+    } else {
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
     }
+
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
+}
+
+static void error(const char* message) {
+    // more often an error will be reported at the token we just consumed
+    errorAt(&parser.previous, message);
+}
+
+static void errorAtCurrent(const char* message) {
+    errorAt(&parser.current, message);
+}
+
+static void advance() {
+    // take old current and store it in previous
+    // so we can get the lexeme after matching a token
+    parser.previous = parser.current;
+
+    for (;;) {
+        parser.current = scanToken();
+        // keep looping until find a non-error token
+        if (parser.current.type != TOKEN_ERROR) break;
+
+        errorAtCurrent(parser.current.start);
+    }
+}
+
+// read and validate next token
+static void consume(TokenType type, const char* message) {
+    if (parser.current.type == type) {
+        advance();
+        return;
+    }
+
+    errorAtCurrent(message);
+}
+
+bool compile(const char* source, Chunk* chunk) {
+    initScanner(source);
+
+    parser.hadError = false;
+    parser.panicMode = false;
+
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression.");
+    return !parser.hadError;
 }
