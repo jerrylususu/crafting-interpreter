d8889ccf90b896db8125f944bcd4c4ff029768b9
Neko Null <jerrylu.mail@qq.com>
2022-05-16 14:56:14 +0300

    18.2 Lox Values and C Values

--- clox/value.h
+++ clox/value.h
@@ -18,6 +18,22 @@ typedef struct {
     } as;
 } Value;
 
+// check Lox value type
+// need to guard before conversion from Lox value to C value
+#define IS_BOOL(value)   ((value).type == VAL_BOOL)
+#define IS_NIL(value)    ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+
+// Lox value -> C value
+// no AS_NIL: only one `nil`, VAL_NIL doesn't carry extra data
+#define AS_BOOL(value)    ((value).as.boolean)
+#define AS_NUMBER(value)  ((value).as.number)
+
+// C value -> Lox Value
+#define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+
 typedef struct {
     int capacity;
     int count;
